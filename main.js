79c718be77c4592d68a9116551371f4aa29bf26e
KNZ <86813245+knz2021@users.noreply.github.com>
2022-10-25 19:24:01 +0900

    Add files via upload
    Fst

---
+++ main.js
@@ -0,0 +1,60 @@
+function clickDisplay1() {
+  const element = document.getElementById("text-button1");
+  element.innerHTML = "1 ペテロ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay2() {
+  const element = document.getElementById("text-button2");
+  element.innerHTML = "2 ヤコブ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay3() {
+  const element = document.getElementById("text-button3");
+  element.innerHTML = "3 ヨハネ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay4() {
+  const element = document.getElementById("text-button4");
+  element.innerHTML = "4 アンデレ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay5() {
+  const element = document.getElementById("text-button5");
+  element.innerHTML = "5 フィリポ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay6() {
+  const element = document.getElementById("text-button6");
+  element.innerHTML = "6 トマス";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay7() {
+  const element = document.getElementById("text-button7");
+  element.innerHTML = "7 ユダ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay8() {
+  const element = document.getElementById("text-button8");
+  element.innerHTML = "8 マタイ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay9() {
+  const element = document.getElementById("text-button9");
+  element.innerHTML = "9 小ヤコブ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay10() {
+  const element = document.getElementById("text-button10");
+  element.innerHTML = "10 シモン";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay11() {
+  const element = document.getElementById("text-button11");
+  element.innerHTML = "11 タダイ";
+  element.style.backgroundColor = "pink";
+}
+function clickDisplay12() {
+  const element = document.getElementById("text-button12");
+  element.innerHTML = "12 バルトロマイ,ナタナエル";
+  element.style.backgroundColor = "pink";
+}
